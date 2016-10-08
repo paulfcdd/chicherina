601033bc861e25607c72ca8c39d09589a4d8f8cc
Paul Novikov <paulfcdd@gmail.com>
2016-10-08 16:20:13 +0200

    adding assets

--- web/app.php
+++ web/app.php
@@ -2,14 +2,31 @@
 include_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
-
+$app['debug'] = true;
 $app
+    ->register(new Silex\Provider\AssetServiceProvider(), [
+        'assets.version' => 'v1',
+        'assets.version_format' => '%s?version=%s',
+        'assets.named_packages' => array(
+            'css' => [
+                        'version' => 'css2',
+                        'base_path' => __DIR__ . '/css',
+                    ],
+            'images' => [
+                        'base_path' => __DIR__ . '/img'
+            ],
+        ),
+    ])
+    ->register(new Silex\Provider\MonologServiceProvider(), [
+        'monolog.logfile' => __DIR__ . '/../logs/dev.log'
+    ])
     ->register(new Silex\Provider\TwigServiceProvider(), [
         'twig.path' => __DIR__ . '/../tpl',
     ]);
 
-$app->get('/', function () use ($app) {
-        return $app['twig']->render('index.html.twig', [
+$app
+    ->get('/', function () use ($app) {
+        return $app['twig']->render('index.twig', [
             'test' => 'Testowy hello',
         ]);
     })
