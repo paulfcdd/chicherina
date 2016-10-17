39fd5ba88ad2c84d614d3ca71b1d0e0ba6745675
Paul Novikov <paulfcdd@gmail.com>
2016-10-17 18:59:10 +0200

    добавление файла конфигурации YML и подключение базы данных

--- web/app.php
+++ web/app.php
@@ -4,6 +4,19 @@ include_once __DIR__ . '/../vendor/autoload.php';
 $app = new Silex\Application();
 $app['debug'] = true;
 $app
+    ->register(new Rpodwika\Silex\YamlConfigServiceProvider(__DIR__ . '/../config/parameters.yml'))
+
+    ->register(new Silex\Provider\DoctrineServiceProvider(), [
+        'db.options' => [
+            'driver' => $app['config']['database']['driver'],
+            'host' => $app['config']['database']['host'],
+            'user' => $app['config']['database']['db_user'],
+            'dbname' => $app['config']['database']['db_name'],
+            'password' => $app['config']['database']['db_password'],
+            'charset'   => 'utf8mb4',
+        ],
+    ])
+
     ->register(new Silex\Provider\AssetServiceProvider(), [
         'assets.version' => 'v1',
         'assets.version_format' => '%s?version=%s',
@@ -19,9 +32,11 @@ $app
             ],
         ),
     ])
+
     ->register(new Silex\Provider\MonologServiceProvider(), [
         'monolog.logfile' => __DIR__ . '/../logs/dev.log'
     ])
+
     ->register(new Silex\Provider\TwigServiceProvider(), [
         'twig.path' => __DIR__ . '/../tpl',
     ]);
@@ -36,8 +51,10 @@ $app
 
 $app
     ->get('/tour', function() use($app) {
+        $sql = 'SELECT * FROM tours';
+        $tours = $app['db']->fetchAll($sql);
         return $app['twig']->render('tour.twig', [
-            
+            'tours' => $tours,
         ]);
     })
     ->bind('tour');
