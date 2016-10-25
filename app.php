16460681f146243afb75b07c1c6a66a62abfc78e
Paul Novikov <paulfcdd@gmail.com>
2016-10-25 23:10:44 +0200

    Управление афишей добавлено + дата-пикер

--- app.php
+++ app.php
@@ -234,9 +234,85 @@ $app
     ->bind('delete_admin');
 
 $app
+    ->post('/delete_tour', function () use ($app) {
+        $id = $_POST['tour_id'];
+        
+        try {
+            $app['db']->delete('tours', ['id' => $id]);
+            return $app->redirect($app["url_generator"]->generate("dashboard"));
+        } catch (\Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    })
+    ->bind('delete_tour');
+
+$app
     ->post('/edit_admin', function() use($app) {
         return new \Symfony\Component\HttpFoundation\Response('kek');
     })
     ->bind('edit_admin');
+
+$app
+    ->post('/add_tour', function(Request $request) use($app) {
+        $status[] = '';
+        $date = trim($request->get('date'));
+        $city = trim($request->get('city'));
+        $place = trim($request->get('place'));
+
+        try {
+            $app['db']->insert(
+                'tours', [
+                    'date' => $date,
+                    'city' => $city,
+                    'location' => $place,
+                ]);
+            $status = [
+                'type' => 'success',
+                'message' => 'Концерт успешно добавлен',
+            ];
+           return new JsonResponse($status);
+
+        } catch (\Exception $e) {
+            $status = [
+                'type' => 'success',
+                'message' => $e->getMessage(),
+            ];
+            return new JsonResponse($status);
+        }
+    })
+    ->bind('add_tour');
+
+$app
+    ->post('/edit_tour', function (Request $request) use($app) {
+        $status[] = '';
+        $id = trim($request->get('id'));
+        $date = trim($request->get('date'));
+        $city = trim($request->get('city'));
+        $place = trim($request->get('place'));
+
+        try {
+            $app['db']->update(
+                'tours', [
+                'date' => $date,
+                'city' => $city,
+                'location' => $place,
+            ], [
+                'id' => $id,
+            ]);
+            $status = [
+                'type' => 'success',
+                'message' => 'Концерт успешно изменен',
+            ];
+            return new JsonResponse($status);
+
+        } catch (\Exception $e) {
+            $status = [
+                'type' => 'success',
+                'message' => $e->getMessage(),
+            ];
+            return new JsonResponse($status);
+        }
+    })
+    ->bind('edit_tour');
 
 $app->run();
--- web/js/dashboard.js
+++ web/js/dashboard.js
@@ -1,5 +1,57 @@
 'use strict';
+
 $(function () {
-    $('#datepicker').datetimepicker();
+    $('#datetimepicker2').datetimepicker({
+        format: 'YYYY-MM-DD',
+        locale: 'ru'
+    });
 });
 
+function addTour(form) {
+    var date = $(form).find("input[id=tourDate]").val();
+    var city = $(form).find("input[id=tourCity]").val();
+    var place = $(form).find("input[id=tourPlace]").val();
+    var path = $(form).find("input[id=path]").val();
+
+    $.ajax({
+        url: path,
+        method: 'post',
+        data: {
+            date: date,
+            city: city,
+            place: place
+        },
+        success: function (data) {
+            $(".info-message").toggleClass('alert-' + data.type);
+            $(".info-message").text(data.message);
+            $(".info-message").show();
+            setTimeout(function () {
+                location.reload, 5000
+            });
+        }
+    });
+}
+
+function editTour(form) {
+    var date = $(form).find("input[id=tourDate]").val();
+    var city = $(form).find("input[id=tourCity]").val();
+    var place = $(form).find("input[id=tourPlace]").val();
+    var path = $(form).find("input[id=path]").val();
+    var id = $(form).find("input[id=id]").val();
+
+    $.ajax({
+        url: path,
+        method: 'post',
+        data: {
+            id: id,
+            date: date,
+            city: city,
+            place: place
+        },
+        success: function (data) {
+            $(".info-message").toggleClass('alert-' + data.type);
+            $(".info-message").text(data.message);
+            $(".info-message").show();
+        }
+    });
+}
